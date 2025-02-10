9166ee1a6285786e87f502383863c8ab37023f06
Ella Neumarker <eneumarker@gmail.com>
2025-02-10 14:22:47 -0700

    Added Form Functionality

--- Mission6Movies/obj/Debug/net8.0/Mission6Movies.AssemblyInfo.cs
+++ Mission6Movies/obj/Debug/net8.0/Mission6Movies.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission6Movies")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da1710f0ade43c41cfe191960d79683d44473c09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef152b6fd2e44d87cae94afdd9e358a03edb3e7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission6Movies")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission6Movies")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
