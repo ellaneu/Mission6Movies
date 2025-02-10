ef152b6fd2e44d87cae94afdd9e358a03edb3e7d
Ella Neumarker <eneumarker@gmail.com>
2025-02-10 14:02:15 -0700

    Views Created

--- Mission6Movies/Controllers/HomeController.cs
+++ Mission6Movies/Controllers/HomeController.cs
@@ -6,26 +6,20 @@ namespace Mission6Movies.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
+    
     public IActionResult Index()
     {
         return View();
     }
 
-    public IActionResult Privacy()
+    public IActionResult GetToKnow()
     {
         return View();
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+    public IActionResult MovieForm()
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return View();
     }
+    
 }
