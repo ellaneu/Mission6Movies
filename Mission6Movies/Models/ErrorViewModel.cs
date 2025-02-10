2ee918b75e84f1ab0be0165d601e2e8f2838ff01
Ella Neumarker <eneumarker@gmail.com>
2025-02-10 13:43:44 -0700

    First Commit

---
+++ Mission6Movies/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Mission6Movies.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
